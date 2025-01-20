f1dbcce7f7b88b023ce35f13feac1b1e41633f6f
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-20 17:48:02 +0530

    p35

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -19,7 +19,7 @@ resource "aws_instance" "web" {
     sudo yum update -y
     sudo yum install -y amazon-ssm-agent
     sudo systemctl enable amazon-ssm-agent
-    sudo systemctl start amazon-ssm-agent
+    sudo systemctl restart amazon-ssm-agent
   EOT
 }
 
