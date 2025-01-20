a42a1b31e10046e6d2e88114fa58e3c031672ac9
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-20 18:53:45 +0530

    p37

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -101,7 +101,12 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
   }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # No inbound access allowed
+  }
   egress {
     from_port   = 0
     to_port     = 0
