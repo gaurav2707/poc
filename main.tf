fce7d3722929a0be710b2f72934a8a9c4459d353
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-21 16:39:52 +0530

    P46

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ module "iam" {
 
 data "aws_ami" "linux-ami" {
   filter {
-    name   = "name"
-    values = ["amzn2-ami-amd-hvm-2.0.20230727.0-x86_64-gp2"]
+    name   = "id"
+    values = ["ami-002acc74c401fa86b"]
 
   }
   filter {
@@ -26,7 +26,7 @@ data "aws_ami" "linux-ami" {
     values = ["hvm"]
   }
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["redhat"]
 }
 
 
