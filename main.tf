727da297e1022fefff1de2a4242962fb7665ed99
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-22 11:40:29 +0530

    p61

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "ec2" {
   source               = "./modules/ec2"
   ami_id               = data.aws_ami.linux-ami.id  # Replace with a valid AMI ID
   instance_type        = "t2.large"
-  security_group_id   = ["sg-0e86157fc4e64160c"]
+  security_group_id   = [module.vpc.security_group_id]
   subnet_id            = module.vpc.subnet_a_id
   iam_instance_profile = module.iam.ec2_instance_profile
   instance_name        = "poc-ec2-instance"
