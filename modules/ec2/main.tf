accb14c1fea725525843e5e2a603ba865ca34208
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-22 08:55:27 +0530

    p51

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
     user_data = <<-EOT
     #!/bin/bash
     sudo yum update -y
-    sudo yum install -y amazon-ssm-agent
+    sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
     sudo systemctl enable amazon-ssm-agent
     sudo systemctl stop amazon-ssm-agent
     sudo systemctl start amazon-ssm-agent
