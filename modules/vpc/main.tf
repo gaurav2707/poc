6b9310cb0771244d26f515234e27e6c49475111d
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-22 11:10:53 +0530

    p60

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -119,7 +119,7 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-# output "security_group_id" {
-#   value = aws_security_group.ec2_sg.id
+output "security_group_id" {
+  value = aws_security_group.ec2_sg.id
   
-# }
+}
