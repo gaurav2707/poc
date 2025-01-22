9e4a7cc65f3b6329bfb3bf7896c383725ca8741d
gaurav2707 <gaurav.5.mishra@coforge.com>
2025-01-22 10:30:53 +0530

    p55

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -2,75 +2,25 @@ provider "aws" {
   region = var.region  # Change to your desired region
 }
 
+########## Role for attaching ec2 instances created  #########
 resource "aws_iam_role" "ssm_role" {
-  name = "EC2-SSMRole"
-  
+  name = "${var.environment}-EC2-SSM-Role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = 
-          Service = [
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = [
           "ec2.amazonaws.com",
           "ssm.amazonaws.com",
           "backup.amazonaws.com"
         ]
       }
-    ]
+      Effect    = "Allow"
+    }]
   })
 }
-
-resource "aws_iam_policy" "ssm_policy" {
-  name        = "SSMPolicy_gaurav"
-  description = "Allow EC2 instances to communicate with Systems Manager"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-				Action = [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-				        "ssm:PutParameter",
-                "ec2messages:*",
-                "ssmmessages:*",
-                "logs:*",
-                "ssm:GetCommandInvocation",
-                "ssm:ListCommands",
-                "ssm:SendCommand",	
-                "ssm:UpdateInstanceInformation"
-        ]        
-
-        Effect = "Allow"
-        Resource = "*"
-      },
-      {
-        Action   = "ec2messages:*"
-        Effect   = "Allow"
-        Resource = "*"
-      },
-      {
-        Action   = "ssm:ListAssociations"
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
+ 
 resource "aws_iam_role_policy_attachment" "ssm_role_policy" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
@@ -87,10 +37,10 @@ resource "aws_iam_role_policy_attachment" "ssm_role_policy_admin" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role       = aws_iam_role.ssm_role.name
   depends_on = [aws_iam_role.ssm_role]
-resource "aws_iam_role_policy_attachment" "ssm_attach" {
-  policy_arn = aws_iam_policy.ssm_policy.arn
-  role       = aws_iam_role.ssm_role.name
 }
+ 
+
+#####################
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "EC2SSMInstanceProfile_gaurav"
